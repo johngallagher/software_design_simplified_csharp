491d8b2b039b53770c40db0eb69b339a0513fc0d
John Gallagher <john@synapticmishap.co.uk>
2023-07-15 21:08:20 +0100

    fix: finally we can log in

--- Controllers/SessionsController.cs
+++ Controllers/SessionsController.cs
@@ -27,19 +27,21 @@ namespace MicropostsApp.Controllers
         [HttpPost]
         public async Task<IActionResult> Login(LoginViewModel model)
         {
+
             if (!ModelState.IsValid)
             {
                 return View(model);
             }
 
-            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: false, lockoutOnFailure: false);
             if (result.Succeeded)
             {
                 return RedirectToAction("Index", "Home");
             }
             else
             {
-                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+
+                ModelState.AddModelError(string.Empty, result.ToString());
                 return View(model);
             }
         }
--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,41 +1,71 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 using MicropostsApp.Data;
 using MicropostsApp.Models;
-using Microsoft.AspNetCore.Authorization;
 
 namespace MicropostsApp.Controllers
 {
     public class UsersController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<User> _userManager;
 
-        public UsersController(ApplicationDbContext context)
+        public UsersController(ApplicationDbContext context, UserManager<User> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         // GET: Users/Create
         public IActionResult Create()
         {
-            var user = new User();
-            return View(user);
+            return View();
         }
 
         // POST: Users/Create
+        // [HttpPost]
+        // [ValidateAntiForgeryToken]
+        // public async Task<IActionResult> Create([Bind("Id,Email,PasswordHash")] User user)
+        // {
+        //     if (ModelState.IsValid)
+        //     {
+        //         _context.Add(user);
+        //         await _context.SaveChangesAsync();
+        //         // Send activation email...
+        //         return RedirectToAction("Index", "Home");
+        //     }
+        //     return View(user);
+        // }
+
+        // Other actions...
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Email,Password")] User user)
+        public async Task<IActionResult> Create(RegisterViewModel model)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(user);
-                await _context.SaveChangesAsync();
-                // Send activation email...
-                return RedirectToAction("Index", "Home");
+                var user = new User { UserName = model.Email, Email = model.Email };
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    var emailConfirmationToken = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+
+                    // Confirm the email
+                    await _userManager.ConfirmEmailAsync(user, emailConfirmationToken);
+
+
+                    return RedirectToAction("Index", "Home");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
             }
-            return View(user);
+            return View(model);
         }
 
-        // Other actions...
     }
 }
