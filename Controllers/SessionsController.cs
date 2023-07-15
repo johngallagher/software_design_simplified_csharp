3d0a7a2e36dd3cb96d0eced1fbddefdea3327efb
John Gallagher <john@synapticmishap.co.uk>
2023-07-15 20:05:18 +0100

    fix: builds but doesn't work

--- Controllers/SessionsController.cs
+++ Controllers/SessionsController.cs
@@ -8,44 +8,69 @@ namespace MicropostsApp.Controllers
 {
     public class SessionsController : Controller
     {
-        private readonly UserManager<IdentityUser> _userManager;
-        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
         private readonly ApplicationDbContext _context;
 
-        public SessionsController(ApplicationDbContext context, UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        public SessionsController(ApplicationDbContext context, UserManager<User> userManager, SignInManager<User> signInManager)
         {
             _context = context;
             _userManager = userManager;
             _signInManager = signInManager;
         }
 
-        // GET: Sessions/Create
-        public IActionResult Create()
+        public IActionResult Login()
         {
-            var user = new User();
-            return View(user);
+            return View();
         }
 
-        // POST: Sessions/Create
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(User user)
+        public async Task<IActionResult> Login(LoginViewModel model)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
-                var result = await _signInManager.PasswordSignInAsync(user.Email, user.PasswordHash, false, lockoutOnFailure: false);
-                if (result.Succeeded)
-                {
-                    return RedirectToAction("Index", "Home");  // Redirect to the home page if login is successful
-                }
-                else
-                {
-                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
-                    return View(user);
-                }
+                return View(model);
             }
-            return View(user);
+
+            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                return View(model);
+            }
         }
+
+        // // GET: Sessions/Create
+        // public IActionResult Create()
+        // {
+        //     var user = new User();
+        //     return View(user);
+        // }
+
+        // POST: Sessions/Create
+        // [HttpPost]
+        // [ValidateAntiForgeryToken]
+        // public async Task<IActionResult> Create(User user)
+        // {
+        //     if (ModelState.IsValid)
+        //     {
+        //         var result = await _signInManager.PasswordSignInAsync(user.Email, user.PasswordHash, false, lockoutOnFailure: false);
+        //         if (result.Succeeded)
+        //         {
+        //             return RedirectToAction("Index", "Home");  // Redirect to the home page if login is successful
+        //         }
+        //         else
+        //         {
+        //             ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+        //             return View(user);
+        //         }
+        //     }
+        //     return View(user);
+        // }
     }
 }
 
---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+public class LoginViewModel
+{
+  [Required]
+  [EmailAddress]
+  public string Email { get; set; }
+
+  [Required]
+  [DataType(DataType.Password)]
+  public string Password { get; set; }
+
+  [Display(Name = "Remember me?")]
+  public bool RememberMe { get; set; }
+}
