152bf3063ad8d0660b7e3483435649b77c319a2e
John Gallagher <john@synapticmishap.co.uk>
2023-07-18 20:48:05 +0100

    feat: add FetchPolicy

--- Extensions/ControllerExtensions.cs
+++ Extensions/ControllerExtensions.cs
@@ -1,5 +1,6 @@
 using Castle;
 using Castle.Messages.Requests;
+using Castle.Messages;
 using MicropostsApp.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,6 +8,39 @@ namespace MicropostsApp.Extensions;
 
 public static class ControllerExtensions
 {
+    public static async Task<Policy> FetchPolicy(
+        this Controller controller,
+        CastleClient castleClient,
+        User? user,
+        string castleRequestToken,
+        string type,
+        string? name = null,
+        string? status = null
+    )
+    {
+        if (user == null)
+            return new Policy(action: ActionType.Allow);
+
+        var response = await castleClient.Risk(
+            request: new ActionRequest
+            {
+                Type = type,
+                Status = status,
+                Name = name,
+                RequestToken = castleRequestToken,
+                Context = Context.FromHttpRequest(
+                    request: controller.Request
+                ),
+                User = new Dictionary<string, object>
+                {
+                    { "id", user.Id },
+                    { "email", user.Email ?? string.Empty }
+                }
+            }
+        );
+        return new Policy(action: response.Policy.Action);
+    }
+
     public static async Task<RiskScore> FetchRiskScore(
         this Controller controller,
         CastleClient castleClient,
