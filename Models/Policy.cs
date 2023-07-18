f9f97a4ebd1246cd598105b144ea8533c275cf60
John Gallagher <john@synapticmishap.co.uk>
2023-07-18 20:30:37 +0100

    feat: new policy class

---
+++ Models/Policy.cs
@@ -0,0 +1,24 @@
+using Castle.Messages;
+using MicropostsApp.Interfaces;
+
+namespace MicropostsApp.Models;
+
+public class Policy : IProtectable
+{
+    private readonly ActionType _action;
+
+    public Policy(ActionType action)
+    {
+        _action = action;
+    }
+
+    public bool Deny()
+    {
+        return _action == ActionType.Deny;
+    }
+
+    public bool Challenge()
+    {
+        return _action == ActionType.Challenge;
+    }
+}
