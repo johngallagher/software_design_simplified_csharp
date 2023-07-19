2cd5789a7c8406983b7a0b9620062ce558a86635
John Gallagher <john@synapticmishap.co.uk>
2023-07-19 22:08:57 +0100

    wip: sketch of Aws Protector

--- Services/Protectors/AWSProtector.cs
+++ Services/Protectors/AWSProtector.cs
@@ -1,6 +1,36 @@
+using Castle;
+using MicropostsApp.Interfaces;
+using MicropostsApp.Models;
+
 namespace MicropostsApp.Services.Protectors;
 
-public class AWSProtector
+public class AwsProtector : Protector
 {
-    
+    private readonly Cloudflare _cloudflare;
+    private readonly CastleClient _client;
+
+    public AwsProtector(Cloudflare cloudflare, CastleClient client)
+    {
+        _cloudflare = cloudflare;
+        _client = client;
+    }
+
+    public Task<Protectable> Protect(
+        Event @event,
+        User? user,
+        string token,
+        HttpContext httpContext
+    )
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task NotifyOf(
+        Event @event,
+        HttpRequest request,
+        UserOperation operation
+    )
+    {
+        throw new NotImplementedException();
+    }
 }
