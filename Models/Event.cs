f2489cf2f781b47a0ee6b7dc532cd1d4f366ab9c
John Gallagher <john@synapticmishap.co.uk>
2023-07-19 18:51:03 +0100

    refactor: new event model

---
+++ Models/Event.cs
@@ -0,0 +1,12 @@
+namespace MicropostsApp.Models;
+
+public enum Event
+{
+    LoginSucceeded,
+    LoginFailed,
+    LoginAttempted,
+    RegistrationSucceeded,
+    RegistrationFailed,
+    RegistrationAttempted,
+    MicropostCreated
+}
