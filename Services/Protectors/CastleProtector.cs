7117b6c6d9147cc3eb2f23d7d09a430693a681e6
John Gallagher <john@synapticmishap.co.uk>
2023-07-19 16:28:39 +0100

    wip: new protector file

---
+++ Services/Protectors/CastleProtector.cs
@@ -0,0 +1,72 @@
+using Castle;
+using Castle.Infrastructure.Exceptions;
+using Castle.Messages;
+using Castle.Messages.Requests;
+using MicropostsApp.Interfaces;
+using MicropostsApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicropostsApp.Services.Protectors;
+
+public class CastleProtector
+{
+    public static async Task<IProtectable> ProtectFromBadActors(
+        Controller controller,
+        CastleClient castleClient,
+        User? user,
+        string castleRequestToken,
+        string type,
+        Cloudflare cloudflare,
+        string? name = null,
+        string? status = null
+    )
+    {
+        try
+        {
+            IProtectable policy;
+            if (user == null)
+                policy = new Policy(action: ActionType.Allow);
+            else
+            {
+                var response = await castleClient.Risk(
+                    request: new ActionRequest
+                    {
+                        Type = type,
+                        Status = status,
+                        Name = name,
+                        RequestToken = castleRequestToken,
+                        Context = Context.FromHttpRequest(
+                            request: controller.Request
+                        ),
+                        User = new Dictionary<string, object>
+                        {
+                            { "id", user.Id },
+                            { "email", user.Email ?? string.Empty }
+                        }
+                    }
+                );
+                policy = new Policy(action: response.Policy.Action);
+            }
+
+            if (policy.Deny())
+            {
+                await cloudflare.Block(context: controller.HttpContext);
+            }
+
+            if (policy.Challenge())
+            {
+                await cloudflare.Challenge(context: controller.HttpContext);
+            }
+
+            return policy;
+        }
+        catch (CastleInvalidTokenException)
+        {
+            return new Policy(action: ActionType.Deny);
+        }
+        catch (CastleExternalException)
+        {
+            return new Policy(action: ActionType.Allow);
+        }
+    }
+}
